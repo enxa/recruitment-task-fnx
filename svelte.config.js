11180bc895fb45be5876638415adb9de4d0b59e7
enxa <enxa@mail.com>
2022-05-29 14:44:48 +0200

    Add basic config, styles and assets

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-auto";
+import preprocess from "svelte-preprocess";
+import path from "path";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocess(),
 	kit: {
-		adapter: adapter(),
-
+    adapter: adapter(),
+    vite: {
+      resolve: {
+        alias: {
+	        $lib: path.resolve("./src/lib"),
+          $components: path.resolve("./src/components"),
+          $animations: path.resolve("./src/animations"),
+        }
+      }
+    },
 		// Override http methods in the Todo forms
 		methodOverride: {
 			allowed: ['PATCH', 'DELETE']
